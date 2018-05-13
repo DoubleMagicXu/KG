fb86fd5accf21f19ec9790fec57634352dd8191e
DoubleMagicXu <2528167446@qq.com>
2018-05-13 23:44:36 +0800

    backup

--- project/divideText.py
+++ project/divideText.py
@@ -1,19 +1,18 @@
 import jieba
-import os
 from collections import Counter
 f=open("../text/投诉文本.txt",'r', encoding='UTF-8')#只读，注意编码方式
+of=open("../text/高频词.txt",'w', encoding='UTF-8')
+#加载停用词表
+stopWord = [line.strip()for line in open('../text/停用词.txt').readlines() ]
 line=f.read() #读取文件一行代码，有可能读的是空行
 if line:
-    #print(line)
     seg_list = jieba.cut(line, cut_all=False)
     c = Counter()
     for x in seg_list:
-        if len(x) > 1 and x != '\r\n':
+        if len(x) > 1 and x != '\r\n'and x not in stopWord:
             c[x] += 1
-    #print("Full Mode: " + "/ ".join(seg_list))
-    for i in c.most_common(1000):
+    for i in c.most_common(1500):
+        of.write(str(i[0])+' '+str(i[1])+'\n')
         print(i)
-    #print(c.most_common(50))
-
-
 f.close()
+of.close()
--- project/extractText.py
+++ project/extractText.py
@@ -25,31 +25,9 @@ for i in range(0,len(list)):
             f.write("\n")
             print(text)
         print(i)
-
-
 f.close()
 
-# soup= BeautifulSoup(open('../html/321188&.htm','rb'))#以rb的方式打开文件
-# #首先提取标题：<H1 class=ts>找到最里面那层<A>
-# f=open("../text/投诉文本.txt","w",encoding='utf-8')#utf-8方式打开
-# #print(soup.prettify())
-# headlist=soup.find_all('h1')
-# for head in headlist:
-#     alist=head.find_all('a')
-#     for a in alist:
-#         if("[咨询投诉]"not in a.string):
-#             f.write(a.string)
-#             print(a.string)
-#
-#
-# #接着提取正文和回复 <TD id=postmessage。。。。>
-# tdlist=soup.find_all('td',attrs={'id':re.compile("postmessage")})
-# for td in tdlist:
-#     text=td.get_text()
-#     f.write(text)
-#     print(text)
 
-# f.close()
 
 
 
