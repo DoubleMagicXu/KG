2e93a6aa9ebf26a92563577c7f1caa2d3ba6f4ac
DoubleMagicXu <2528167446@qq.com>
2018-05-18 13:13:04 +0800

    backup

--- project/dataVisualization.py
+++ project/dataVisualization.py
@@ -4,24 +4,13 @@ plt.rcParams['axes.unicode_minus']=False#解决中文乱码问题
 file=open("../text/高频词.txt",'r',encoding='UTF-8')
 eef=open("../text/实体关联.txt",'r',encoding='UTF-8')
 ees=open("../text/实体关联排序.txt",'r',encoding='UTF-8')
-# eline=file.readline()
-# edic={}
-# while eline:
-#     text = eline.strip('\n')
-#     edic[text.split()[0]] = text.split()[1]
-#     eline = file.readline()
-# freq=[]
-# for i in edic.values():
-#     freq.append(int(i))
-# plt.bar(range(len(edic)),freq,color='gray')
-# plt.show()
+
 eesdic={}
 eesline=ees.readline()
 while eesline:
     text=eesline.strip('\n')
     eesdic[(text.split()[0],text.split()[1])]=text.split()[2]
     eesline=ees.readline()
-
 freq=[]
 k=0
 for i in eesdic.values():
@@ -32,8 +21,6 @@ for i in eesdic.values():
 
 plt.bar(range(len(freq)),freq,color='gray')
 plt.show()
-
-
 file.close()
 eef.close()
 ees.close()
---
+++ project/evaluate.py
@@ -0,0 +1,23 @@
+import random
+file=open("../text/实体关联排序.txt",'r',encoding='UTF-8')
+dic={}#存放数据的字典
+line=file.readline()
+i=0
+while(line):
+    text=line.strip('\n')
+    dic[(text.split()[0],text.split()[1])]=text.split()[2]
+    line=file.readline()
+    i+=1
+    if i>=10000:
+        break
+html = ""
+for w, c in dic.items():
+    color = 'rgb(%s, %s, %s)' % (str(random.randint(0, 255)), str(random.randint(0, 255)), str(random.randint(0, 255)))
+    fontsize = int(float(c) * 1 + 10)
+    html += '<span style=\"font-size:' + str(fontsize) + 'px;color:' + color + ';float:left;\">' +'&#160'+ w[0]+'-'+w[1]+'&#160' + '</span>'
+
+# dump it to a file
+f=open('../text/result.html','w',encoding='UTF-8')
+f.write(html)
+print(html)
+f.close()
